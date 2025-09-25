3536f80631e4544ef1beb66e3acbfb8c6914fd40
Marcio Costa <marcio-costa38@hotmail.com>
2025-09-25 20:29:20 -0300

    criando rota Home respondendo por JSON

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using minimal_api.Dominio.DTOs;
 using minimal_api.Dominio.Interfaces;
+using minimal_api.Dominio.ModelViews;
 using minimal_api.Dominio.Servicos;
 using minimal_api.Infraestrutura.Db;
 
@@ -22,7 +23,7 @@ builder.Services.AddDbContext<DbContexto>(options =>
 var app = builder.Build();
 
 
-app.MapGet("/", () => "Olá Galera top!");
+app.MapGet("/", () => Results.Json(new Home()));
 
 
 app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
