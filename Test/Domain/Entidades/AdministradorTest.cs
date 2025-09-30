0a5dc831f45ec68c12e2e9a4ec6b50c30e8949f8
Marcio Costa <marcio-costa38@hotmail.com>
2025-09-29 23:42:36 -0300

    criando teste de unidade

---
+++ Test/Domain/Entidades/AdministradorTest.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using minimal_api.Dominio.Entidades;
+
+namespace Test.Domain.Entidades
+{
+    [TestClass]
+    public class AdministradorTest
+    {
+        [TestMethod]
+        public void TestarGetSetPropriedades()
+        {
+            //Arrange - todas as variaveis criadas para fazer validacoes...
+            var adm = new Administrador();
+
+            //Act - todas as ações que vamos fazer...
+            adm.Id = 1;
+            adm.Email = "teste@test.com";
+            adm.Senha = "teste";
+            adm.Perfil = "Adm";
+
+            //Assert - onde é feita a validação dos dados...
+            Assert.AreEqual(1, adm.Id);
+            Assert.AreEqual("teste@test.com", adm.Email);
+            Assert.AreEqual("teste", adm.Senha);
+            Assert.AreEqual("Adm", adm.Perfil);
+            
+        }
+    }
+}
