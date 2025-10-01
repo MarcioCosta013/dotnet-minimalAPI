d55290a6d9e5f3d8bcbea24d1126cb368d2871f3
Marcio Costa <marcio-costa38@hotmail.com>
2025-10-01 18:07:29 -0300

    refatorando o Program.cs para o mockar depois

---
+++ Test/Requests/AdministradorRequestTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Requests
+{
+    
+    public class AdministradorRequestTest
+    {
+        [TestMethod]
+        public void TestarGetSetPropriedades()
+        {
+            //Arrange - todas as variaveis criadas para fazer validacoes...
+            
+            //Act - todas as ações que vamos fazer...
+            
+            //Assert - onde é feita a validação dos dados...
+            
+        }
+    }
+}
